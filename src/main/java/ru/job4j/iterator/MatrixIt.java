1026662e5ae18c661bf1c919f66a8840b9f3a892
evgenshegai <evgeniishegai@yandex.ru>
2021-02-08 18:24:35 +1000

    Итератор для двухмерного массива int[][]

--- src/main/java/ru/job4j/iterator/MatrixIt.java
+++ src/main/java/ru/job4j/iterator/MatrixIt.java
@@ -5,52 +5,49 @@ import java.util.NoSuchElementException;
 
 public class MatrixIt implements Iterator<Integer> {
 
-    private int size;
-    private int position = 0;
-    private int row = 0;
-    private int col = 0;
-    private int[][] matrix;
-    private int element;
+    private int[][] array;
+    private int row;
+    private int col;
 
-    public MatrixIt(int[][] matrix) {
-        this.matrix = matrix;
-        this.size = countElements(matrix);
-    }
-
-    private int countElements(int[][] matrix) {
-        int count = 0;
-        for (int[] row : matrix) {
-            count += row.length;
-        }
-        return count;
+    public MatrixIt(int[][] array) {
+        this.array = array;
     }
 
     @Override
     public boolean hasNext() {
-        return position < size;
+        int locRow = row;
+        boolean result = false;
+        int count = 0;
+        while (array[locRow].length == 0) {
+            locRow++;
+            count++;
+            if (count == array.length) {
+                return false;
+            }
+        }
+        if (array[locRow].length != 0) {
+            result = true;
+            locRow--;
+        }
+        return result;
     }
 
     @Override
     public Integer next() {
-        if (position >= size) {
+        if (!hasNext()) {
             throw new NoSuchElementException();
         }
-        try {
-            element = matrix[row][col];
-        } catch (IndexOutOfBoundsException e) {
-           element = matrix[++row][col];
-           row--;
+        if (array[row].length == 0) {
+            while (array[row].length == 0) {
+                row++;
+                col = 0;
+            }
         }
-        position++;
-        col++;
-        check();
-        return element;
-    }
-
-    private void check() {
-        while (row < matrix.length && col >= matrix[row].length) { //для того, чтоб пропустить возможные "пустые" строки
-            col = 0;
+        Integer result = array[row][col++];
+        if (col >= array[row].length) {
             row++;
+            col = 0;
         }
+        return result;
     }
 }
