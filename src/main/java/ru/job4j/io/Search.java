87aeeb1c3b56988f14f32c70e1198bae5652d3c4
evgenshegai <evgeniishegai@yandex.ru>
2021-02-23 17:51:42 +1000

    Валидация параметров запуска

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -13,6 +13,9 @@ public class Search {
     }
 
     public static void main(String[] args) throws IOException {
+        if (args.length != 2 && args[0] != null && args[1] != null) {
+            throw new IllegalArgumentException("Wrong arguments");
+        }
         SearchFiles searcher = new SearchFiles(x -> x.endsWith(args[1]));
         Files.walkFileTree(Paths.get(args[0]), searcher);
         searcher.getList().stream().forEach(x -> System.out.println(x.toFile().getName()));
