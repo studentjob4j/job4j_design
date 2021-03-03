e4603236365122dec07a0db7d06c712ac19e17c2
evgenshegai <evgeniishegai@yandex.ru>
2021-03-03 10:36:45 +1000

    Бот доработанный вариант

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -1,10 +1,12 @@
 package ru.job4j.io;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,28 +16,18 @@ public class EchoServer {
 
     public static void main(String[] args) {
         String str = null;
+
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
+                int count = 0;
                 Socket socket = server.accept();
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
                      while (in.ready()) {
                          str = in.readLine();
-                         if (str.contains("Exit")) {
-                             out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                             out.write("GoodBye My Dear friend\r\n\r\n".getBytes());
-                             server.close();
-                         } else if (str.contains("Hello")) {
-                             out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                             out.write("Hello my dear friend\r\n\r\n".getBytes());
-                             break;
-                         } else if (str.contains("Any")) {
-                             out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                             out.write("What\r\n\r\n".getBytes());
-                             break;
-                         } else {
-                             out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                             break;
+                         if (count == 0) {
+                             writeInOut(str, out, server);
+                             count = 1;
                          }
                         System.out.println(str);
                      }
@@ -45,5 +37,21 @@ public class EchoServer {
            // e.printStackTrace();
             LOG.error("Exception in streams", e);
         }
+    }
+
+    private static void writeInOut(String value, OutputStream out, ServerSocket server) throws IOException {
+        List<String> list = List.of("HTTP/1.1 200 OK\r\n\r\n", "GoodBye My Dear friend\r\n\r\n", "Hello my dear friend\r\n\r\n",
+                "What\r\n\r\n");
+        out.write(list.get(0).getBytes());
+            if (value.contains("Exit")) {
+                out.write(list.get(1).getBytes());
+                server.close();
+            }
+            if (value.contains("Hello")) {
+                out.write(list.get(2).getBytes());
+            }
+            if (value.contains("Any")) {
+                out.write(list.get(3).getBytes());
+            }
     }
 }
