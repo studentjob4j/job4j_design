bfc3e6c33a1aa188bebfc8588bfe9dbd76943603
evgenshegai <evgeniishegai@yandex.ru>
2021-03-05 15:04:34 +1000

    JAXB. Преобразование XML в POJO

--- src/main/java/ru/job4j/serialization/Contact.java
+++ src/main/java/ru/job4j/serialization/Contact.java
@@ -1,34 +1,30 @@
 package ru.job4j.serialization;
 
+import com.sun.xml.txw2.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlAttribute;
 import java.io.*;
 import java.nio.file.Files;
 
+@XmlElement(value = "contact")
 public class Contact implements Serializable {
 
-    private static final long serialVersionUID = 1L;
-    private final int zipCode;
-    private final String phone;
+    @XmlAttribute
+    private String phone;
 
-    public Contact(int zipCode, String phone) {
-        this.zipCode = zipCode;
+    public Contact(String phone) {
         this.phone = phone;
     }
 
-    public int getZipCode() {
-        return zipCode;
-    }
-
-    public String getPhone() {
-        return phone;
+    public Contact() {
     }
 
     @Override
     public String toString() {
-        return "Contact{" + "zipCode=" + zipCode + ", phone='" + phone + '\'' + '}';
+        return "Contact{" + "phone='" + phone + '\'' + '}';
     }
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        final Contact contact = new Contact(123456, "+7 (111) 111-11-11");
+        final Contact contact = new Contact("+7 (111) 111-11-11");
         /* Запись объекта в файл */
         File tempFile = Files.createTempFile(null, null).toFile();
         try (FileOutputStream fos = new FileOutputStream(tempFile);
--- src/main/java/ru/job4j/serialization/JsonExample.java
+++ src/main/java/ru/job4j/serialization/JsonExample.java
@@ -1,41 +1,70 @@
 package ru.job4j.serialization;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.annotation.*;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.util.Arrays;
 
+@XmlRootElement(name = "jsonexample")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class JsonExample {
 
+    @XmlAttribute
     private boolean value;
+    @XmlAttribute
     private int data;
+    @XmlAttribute
     private String text;
     private Contact contact;
-    private String[] array;
+    @XmlElementWrapper(name = "arrays")
+    @XmlElement(name = "array")
+    private static String[] arrays;
 
-    public JsonExample(final boolean value, final int data, final String text, final Contact contact, final String[] array) {
+    public JsonExample(boolean value, int data, String text, Contact contact, String[] array) {
         this.value = value;
         this.data = data;
         this.text = text;
         this.contact = contact;
-        this.array = array;
+        arrays = array;
+    }
+
+    public JsonExample() {
     }
 
     @Override
     public String toString() {
         return "JsonExample{" + "value=" + value + ", data=" + data + ", text='" + text + '\'' + ", contact=" + contact
-                + ", array=" + Arrays.toString(array) + '}';
+                + ", array=" + Arrays.toString(arrays) + '}';
     }
 
-    public static void main(String[] args) {
-        String[] array = new String[] {"text", "data", "value"};
-        final JsonExample example = new JsonExample(true, 2021, "Thursday",
-                new Contact(12345, "1122"), array);
-        final Gson gson = new GsonBuilder().create();
-        // создал json из объекта JsonExample
-        String temp = gson.toJson(example);
-        System.out.println(temp);
-        // создал объект из json
-        final JsonExample jsonExample = gson.fromJson(temp, JsonExample.class);
-        System.out.println(jsonExample);
+    public static void main(String[] args) throws JAXBException, IOException {
+        JsonExample example = new JsonExample(true, 2021, "Thursday",
+                new Contact("1122"), new String[] {"one", "two", "three"});
+        // Получаем контекст для доступа к АПИ
+        JAXBContext context = JAXBContext.newInstance(JsonExample.class);
+        // Создаем сериализатор
+        Marshaller marshaller = context.createMarshaller();
+        // Указываем, что нам нужно форматирование
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        String xml = "";
+        try (StringWriter writer = new StringWriter()) {
+            // Сериализуем
+            marshaller.marshal(example, writer);
+            xml = writer.getBuffer().toString();
+            System.out.println(xml);
+        }
+        //  создаем десериализатор
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        try (StringReader reader = new StringReader(xml)) {
+            // десериализуем
+            JsonExample result = (JsonExample) unmarshaller.unmarshal(reader);
+            System.out.println(result);
+        }
+
     }
 }
