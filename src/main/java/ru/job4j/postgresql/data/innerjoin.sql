f441cde0929f07dfb4d1346e36ffead6f5bd456f
evgenshegai <evgeniishegai@yandex.ru>
2021-03-11 15:17:01 +1000

    Inner join

---
+++ src/main/java/ru/job4j/postgresql/data/innerjoin.sql
@@ -0,0 +1,36 @@
+create table profession (
+                            id serial primary key,
+                            description text,
+                            salary int
+);
+
+select * from profession;
+
+create table person (
+                        id serial primary key,
+                        name varchar(20),
+                        age int,
+                        address varchar(30),
+                        profession_id int references profession(id)
+);
+
+select * from person;
+
+insert into profession(description, salary) VALUES ('Проджект менеджер', 200);
+insert into profession(description, salary) VALUES ('Девопс', 150);
+insert into profession(description, salary) VALUES ('Программист', 100);
+insert into profession(description, salary) VALUES ('Маркетолог', 130);
+select * from profession;
+
+insert into person(name, age, address, profession_id) VALUES ('Василий', 32, 'Владивосток', 2);
+insert into person(name, age, address, profession_id) VALUES ('Николай', 22, 'Владивосток', 1);
+insert into person(name, age, address, profession_id) VALUES ('Евгений', 35, 'Владивосток', 3);
+insert into person(name, age, address, profession_id) VALUES ('Юлия', 32, 'Владивосток', 4);
+insert into person(name, age, address, profession_id) VALUES ('Петр',null,'Владивосток', null);
+select * from person;
+
+
+select p.name ,p.age, p.address from person as p join profession as pr on  p.id = pr.id;
+select p.name ,p.age from person as p full join profession as pr on  p.id = pr.id;
+select p.address from person as p join profession as pr on p.id = pr.id;
+
